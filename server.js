b5121d84cd9514d324b76fcd5d12f2e05433368e
George Harrison <g.a.harrison91@gmail.com>
2023-03-12 14:26:17 -0600

    added server.js code

--- server.js
+++ server.js
@@ -1,13 +1,36 @@
 // requirements
-
 const express = require("express");
+const api = require("./Routes/index");
+const path = require("path");
+
+const PORT = process.env.PORT || 3001;
+
+const app = express();
 
 // Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use("/api", api);
+
+app.use(express.static("public"));
 
 // Routes-
 
 // Homepage Route
+app.get("/", (req, res) =>
+  res.sendFile(path.join(__dirname, "/public/index.html"))
+);
 
 // Notes Page Route
+app.get("/notes", (req, res) =>
+  res.sendFile(path.join(__dirname, "/public/notes.html"))
+);
 
 // Default Route (needed?)
+app.get("*", (req, res) =>
+  res.sendFile(path.join(__dirname, "/public/index.html"))
+);
+
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT} 🚀`)
+);
