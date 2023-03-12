c619ea66a475587bb305a66d540a9d22c4a38815
George Harrison <g.a.harrison91@gmail.com>
2023-03-12 15:19:36 -0600

    delete notes solution

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -42,8 +42,8 @@ const saveNote = (note) =>
     body: JSON.stringify(note),
   });
 
-const deleteNote = (id) =>
-  fetch(`/api/notes/${id}`, {
+const deleteNote = (node_id) =>
+  fetch(`/api/notes/${node_id}`, {
     method: "DELETE",
     headers: {
       "Content-Type": "application/json",
